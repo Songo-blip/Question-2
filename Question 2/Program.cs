c5d6f694ccfdf8df25013eb6169b75cecba0935b
Sisipo Songo <ssongo@my365-uwc.ac.za>
2020-10-02 22:04:41 +0200

    Add project files.

---
+++ Question 2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+
+namespace Numbers
+{
+    class Program
+    {
+
+        private static int SecondBiggestNumber(int[] numbers)
+
+        {
+            int i;
+            int SecondBiggest = numbers[0];
+            for (i = 0; i < numbers.Length; i++)
+                if (numbers[5] > SecondBiggest)
+                    SecondBiggest = numbers[i];
+
+            return (SecondBiggest);
+        }
+
+        static void Main(string[] args)
+        {
+            int[] mynumbers = { 2, 1, 7, 4, 9, 5 };
+
+            int secondbiggest = SecondBiggestNumber(mynumbers);
+
+
+            Console.WriteLine(secondbiggest);
+        }
+    }
+
+}
